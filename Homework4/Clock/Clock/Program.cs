1e329959a7d47559e3cf69d94020ce4e6ac682e2
zzqzzqzzq0816 <50022832+zzqzzqzzq0816@users.noreply.github.com>
2020-03-15 14:17:04 +0800

    第四次Homework

---
+++ Homework4/Clock/Clock/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Clock
+{
+    public delegate void ClockHandler(Clock clock, ClockEventArgs args);
+    public class ClockEventArgs
+    {
+        public int Hour { get; set; }
+        public int Min { get; set; }
+        public  int Sec { get; set; }
+    }
+    public class Clock
+    {
+        public event ClockHandler OnTick;
+        public event ClockHandler OnAlarm;
+        public int AlarmHour { get; set; }
+        public int AlarmMin { get; set; }
+        public int AlarmSec { get; set; }
+        public void InitClock(int hour,int min,int sec)
+        {
+            Console.WriteLine("init clock with " + hour + ":" + min + ":" + sec);
+            ClockEventArgs args = new ClockEventArgs() { Hour=hour,Min=min,Sec=sec };
+            while (true)
+            {
+                OnTick(this, args);
+                OnAlarm(this, args);
+                Thread.Sleep(1000);
+            }
+        }
+        
+    }
+    public class Person
+    {
+        public Person()
+        {
+            clock.OnTick += ClockOnTick;
+            clock.OnAlarm += ClockOnAlarm;
+        }
+        public Clock clock = new Clock();
+        public void SetAlarm(int hour,int min,int sec)
+        {
+            clock.AlarmHour = hour;
+            clock.AlarmMin = min;
+            clock.AlarmSec = sec;
+        }
+        //在用户按下闹钟开关后，闹钟才可以开始走
+        public void ClockOnTick(Clock clock,ClockEventArgs args)
+        {
+            args.Sec++;
+            if (args.Sec == 60)
+            {
+                args.Sec = 0;
+                args.Min++;
+            }
+            if (args.Min == 60)
+            {
+                args.Min = 0;
+                args.Hour = 0;
+            }
+            if(args.Hour==24)
+            {
+                args.Hour = 0;
+            }
+            Console.WriteLine("Tick..." + args.Hour + ":" + args.Min + ":" + args.Sec);
+        }
+        public void ClockOnAlarm(Clock clock,ClockEventArgs args)
+        {
+            if (clock.AlarmHour == args.Hour && clock.AlarmMin == args.Min && clock.AlarmSec == args.Sec)
+            {
+                Console.WriteLine("Alarm..." + args.Hour + ":" + args.Min + ":" + args.Sec);
+            }
+        }
+        public void Start(int hour,int min,int sec)
+        {
+            clock.InitClock(hour,min,sec);
+        }
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+            person.SetAlarm(3, 2, 25);
+            person.Start(3, 2, 10);
+        }
+    }
+}
---
+++ Homework4/TestAction/TestAction/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestAction
+{
+
+    // 链表节点
+    public class Node<T>
+    {
+        public Node<T> Next { get; set; }
+        public T Data { get; set; }
+
+        public Node(T t)
+        {
+            Next = null;
+            Data = t;
+        }
+    }
+
+    //泛型链表类
+    public class GenericList<T>
+    {
+        private Node<T> head;
+        private Node<T> tail;
+
+        public GenericList()
+        {
+            tail = head = null;
+        }
+
+        public Node<T> Head
+        {
+            get => head;
+        }
+
+        public void Add(T t)
+        {
+            Node<T> n = new Node<T>(t);
+            if (tail == null)
+            {
+                head = tail = n;
+            }
+            else
+            {
+                tail.Next = n;
+                tail = n;
+            }
+        }
+        public void ForEach(Action<T> action)
+        {
+            Node<T> t = this.head;
+            while (t != null)
+            {
+                action(t.Data);
+                t = t.Next;
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GenericList<int> intlist = new GenericList<int>();
+            for (int x = 0; x < 10; x++)
+            {
+                intlist.Add(x);
+            }
+            intlist.ForEach(x => Console.Write(x));
+            Console.WriteLine();
+            int max = int.MinValue;
+            intlist.ForEach(x => { if (max < x) max = x; });
+            int min = int.MaxValue;
+            intlist.ForEach(x => { if (min > x) min = x; });
+            int sum = 0;
+            intlist.ForEach(x => { sum += x; });
+            Console.WriteLine("max" + max + "min" + min + "sum" + sum);
+        }
+    }
+}
